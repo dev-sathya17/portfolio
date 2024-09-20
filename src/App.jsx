9f4a0fde87498a84a9c69af85ea306b54a6bf9d6
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-09-20 15:40:14 +0530

    initial project setup with bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="">
+      <h1>Hello, Bootstrap with Vite!</h1>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+};
+
+export default App;
