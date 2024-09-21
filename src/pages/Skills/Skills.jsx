42e0c4da90436bc3fa1a2dd5d71575e9a5f1c992
Sathyanarayanan V <dev.sathya1701@gmail.com>
2024-09-21 11:21:09 +0530

    carousel added to skills section

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -2,31 +2,64 @@ import { skillsData } from "../../data/skills";
 import "./Skills.css";
 const Skills = () => {
   return (
-    <section className="skills">
+    <>
       <h1 className="skills-title">Skills</h1>
-      <div className="d-flex justify-content-between flex-wrap">
-        {skillsData.map((skill, index) => {
-          return (
-            <div key={index} className="skill">
-              <p>{skill.title}</p>
-              <hr className="line" />
-              <div className="skill-list mt-4">
-                {skill.skills.map((item, index) => (
-                  <div key={index} className="pill">
-                    <img
-                      className="skill-img"
-                      src={item.icon}
-                      alt={item.name}
-                    />
-                    <li key={item.id}>{item.name}</li>
-                  </div>
-                ))}
+      <section
+        id="carouselExample"
+        className="skills carousel slide m-5"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-inner">
+          {skillsData.map((skill, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <div className="skill-wrapper">
+                <p>{skill.title}</p>
+                <hr className="line" />
+                <div className="skill-list mt-4">
+                  {skill.skills.map((item, index) => (
+                    <div key={index} className="pill">
+                      <img
+                        className="skill-img"
+                        src={item.icon}
+                        alt={item.name}
+                      />
+                      <li key={item.id}>{item.name}</li>
+                    </div>
+                  ))}
+                </div>
               </div>
             </div>
-          );
-        })}
-      </div>
-    </section>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </section>
+    </>
   );
 };
 
